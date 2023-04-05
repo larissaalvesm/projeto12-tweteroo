ef5f2eae1e2a02599a0cda67b92cf4f32b4d479e
larissa <larissaalves1102@gmail.com>
2023-04-04 23:52:01 -0300

    Requisito GET tweets inicializado

--- src/app.js
+++ src/app.js
@@ -4,4 +4,20 @@ const PORT = 5000;
 const app = express();
 app.listen(PORT);
 
+const tweets = [];
 
+const tweetsRetornados = [];
+
+app.get("/tweets", (req, res) => {
+
+    if (tweets.length === 0) {
+        res.send([]);
+    } else if (tweets.length <= 10) {
+        res.send(tweets);
+    } else {
+        for (let i = tweets.length - 1; tweetsRetornados.length <= 10; i--) {
+            tweetsRetornados.push(tweets[i]);
+        }
+        res.send(tweetsRetornados);
+    }
+})
