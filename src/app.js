d2cf395b7ebbd15fb1adfc17e19c199db16521d0
larissa <larissaalves1102@gmail.com>
2023-04-05 23:35:47 -0300

    Requisito POST sign-up implementado

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors"
 
 const PORT = 5000;
 const app = express();
+app.use(cors())
+app.use(json())
 app.listen(PORT);
 
-const tweets = [];
+const tweets = [
+    {
+        username: "bobesponja",
+        tweet: "Eu amo hambúrguer de siri!"
+    }
+];
 
 const tweetsRetornados = [];
+
+const usuarios = [
+    {
+        username: 'bobesponja',
+        avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+    }
+];
 
 app.get("/tweets", (req, res) => {
 
@@ -20,4 +35,14 @@ app.get("/tweets", (req, res) => {
         }
         res.send(tweetsRetornados);
     }
+})
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    const novoUsuario = {
+        username,
+        avatar
+    }
+    usuarios.push(novoUsuario);
+    res.send("OK");
 })
